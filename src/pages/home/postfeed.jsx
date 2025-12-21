2d6694576446fc02f7b0bffd0319d5b9ff33453b
EmekaBest <josephemeka2611@gmail.com>
2025-12-21 01:23:55 +0100

    Work in progress:: developing the postfeed section

--- src/pages/home/postfeed.jsx
+++ src/pages/home/postfeed.jsx
@@ -1,14 +1,34 @@
-import { StyleSheet, View } from "react-native"
+import { Image, StyleSheet, Text, View } from "react-native"
+import { useAuth } from "../../AuthContext";
 
 
 
 const PostFeed = () => {
 
-return(
-    <View style = {styles.container}>
+    const { token, user } = useAuth();
 
-    </View>
-)
+    
+
+    return(
+        <View style = {styles.container}>
+            <View style = {styles.containerTop}>
+                <View style = {styles.containerTopImage}>
+                    <Image
+                        source={{uri: user.avatar}}
+                        style={{ height: "100%", width: "100%" }}
+                        resizeMode="cover"
+                    />
+                </View>
+                <View style = {styles.containerTopTextContainer}>
+                    <Text style = {styles.containerTopTextContainer_Text}>What is on your mind? #Hashtag... {"\n"} @Mention.. Link..  </Text>
+                </View>
+
+            </View>
+            <View style = {styles.containerBottom}></View>
+
+
+        </View>
+    )
 }
 
 
@@ -17,8 +37,39 @@ const styles = StyleSheet.create({
 
     container:{
         marginHorizontal:10,
-        height:700,
-        backgroundColor:"lightgreen",
+        height:200,
+    },
+
+    containerTop:{
+        height:"70%",
+        display:"flex",
+        flexDirection:"row",
+        alignItems:"center",
+        paddingHorizontal:15,
+    },
+
+    containerTopImage:{
+        height:50,
+        width:50,
+        borderRadius:50,
+        overflow:"hidden"
+    },
+
+    containerTopTextContainer:{
+        marginLeft:10
+
+    },
+
+    containerTopTextContainer_Text:{
+        fontSize:16,
+        color:"#848484ff"
+
+
+    },
+
+    containerBottom:{
+        height:"30%",
+        backgroundColor:"#f0f0f0ff"
     }
 
 })
