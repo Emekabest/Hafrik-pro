ca0c0273be2e3188964003fe2c8f7bc44c5db172
EmekaBest <josephemeka2611@gmail.com>
2025-12-23 17:37:52 +0100

    Still working on the uploadmediacontroller

--- src/controllers/uploadmediacontroller.js
+++ src/controllers/uploadmediacontroller.js
@@ -2,47 +2,44 @@ import axios from "axios";
 import { useAuth } from "../AuthContext";
 
 
-
 const UploadMediaController = async(media, token)=>{
-    console.log("Ok")
 
     const API_URL = `https://hafrik.com/api/v1/uploads/media.php`;
 
     try{
-        
           const formData = new FormData();
 
           formData.append('image', {
-            uri:media.uri,
-            type:"image/jpeg",
-            name:"photo.jpg"
+            uri: media.uri,
+            type: 'image/jpeg',
+            name: media.fileName || `photo_${Date.now()}.jpg`
           });
 
           const response = await axios.post(API_URL, formData, {
             headers: {
-                Authorization: `Bearer${token}`,
+                Authorization: `Bearer ${token}`,
             },
-          })
+        })
 
           console.log(response.data)
-
+          return response.data;
     }
     catch(error){
 
-        // console.log("An error occured::", error)
+        console.log("An error occured::", error)
+        console.log("resopnse::"+error.response)
+        console.log("request::"+error.request)
 
-        if (error.response) {
-  console.log('Server responded:', error.response.data);
-} else if (error.request) {
-  console.log('Request made but no response');
-} else {
-  console.log('Axios config error:', error.message);
-}
+
+        if (error.response){
+            console.log('Server responded:', error.response.data);
+        } else if (error.request) {
+            console.log('Request made but no response');
+        } else {
+            console.log('Axios config error:', error.message);
+        }
 
     }
-
-
-
 
 }
 
