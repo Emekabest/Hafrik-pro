8650aea9588131fe88bf74dcd9edcfb835e9902f
EmekaBest <josephemeka2611@gmail.com>
2026-01-17 23:49:12 +0100

    Working on flatlist--2

--- src/pages/reels/reelcard.jsx
+++ src/pages/reels/reelcard.jsx
@@ -1,7 +1,15 @@
-import { StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View, StatusBar } from "react-native";
 import ReelMedia from "./reelmedia";
 import ReelInteractionContainer from "./reelinteractioncontainer";
 import { memo } from "react";
+// import { StatusBar } from "expo-status-bar";
+
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+const statusBarHeight = Math.floor(StatusBar.currentHeight) || 0;
+
+console.log("Status Bar Height:", statusBarHeight);
+
 
 
 const ReelCard = ({ reel }) => {
@@ -24,7 +32,7 @@ const ReelCard = ({ reel }) => {
 const styles = StyleSheet.create({
 
     container:{
-        height: "100%",
+        height: SCREEN_HEIGHT - statusBarHeight + 1,
         width: '100%',
         backgroundColor: '#c7a391ff'
     }
