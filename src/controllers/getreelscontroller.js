700b31ec0f601d182cc6cd4b3a133b3943c0ca69
EmekaBest <josephemeka2611@gmail.com>
2026-01-17 15:57:02 +0100

    Woking on the reels

---
+++ src/controllers/getreelscontroller.js
@@ -0,0 +1,29 @@
+import axios from "axios"
+
+
+const GetReelsController = async(token) => {
+
+    try{
+
+        const response = await axios.get(`https://hafrik.com/api/v1/feed/list.php?page=1`, {
+            headers:{
+                    Authorization: `Bearer ${token}`,
+                    'Cache-Control': 'no-cache'
+            }
+        })
+
+        // console.log("This is resonse::"+response.data.data.data)
+
+        return {status: response.status, data: response.data.data.data};
+    }
+    catch(error){
+
+        console.log("Error in GetReelsController:", error.message);
+        return {status: error.response ? error.response.status : 500, data: null};
+
+    }
+
+}
+
+
+export default GetReelsController;
--- src/pages/reels/reels2.jsx
+++ src/pages/reels/reels2.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import GetReelsController from "../../controllers/getreelscontroller";
+import { useAuth } from "../../AuthContext";
+
+
 
 
 const Reels2 = () => {
+    const { token } = useAuth();
+
+    const [reels, setReels] = useState([]);
+
+
+    useEffect(()=>{
+        const getReelsData = async()=>{
+            const response = await GetReelsController(token)
+
+            if (response.status === 200){
+                setReels(response.data);
+            }
+        }
+        getReelsData();////
+    },[])
+
+
+    
 
     return(
         <View>
-            <Text>Reels2 Page</Text>
+            <Text></Text>
         </View>
     )
 
